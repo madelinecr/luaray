f9418f3ac30088707374374b6696be29cb926e6e
Blaine Pace <blainepace@gmail.com>
2014-04-02 01:44:01 -0700

    Third sphere for test.lua

--- test.lua
+++ test.lua
@@ -3,10 +3,12 @@ c = Camera(1280, 800, s)
 
 sphere1 = sphere(150,500,20,35,2,3,1)
 sphere2 = sphere(300,500,20,95,0,1,2)
-alight = light(Vec3(300,-500,20), 8)
+sphere3 = sphere(450,450,20,45,0,1,2)
+alight = light(Vec3(80,500,20), 8)
 
 s:add_sphere(sphere1)
 s:add_sphere(sphere2)
+s:add_sphere(sphere3)
 s:add_light(alight)
 
 c:render()
