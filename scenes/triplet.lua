615607ae52b51ade5e6d265603777c0c0d4e3fc8
Blaine Pace <blainepace@gmail.com>
2014-08-15 08:33:03 -0700

    Increased light2 strength in triplet scene

--- scenes/triplet.lua
+++ scenes/triplet.lua
@@ -5,7 +5,7 @@ sphere1 = sphere(Vec3(550,300,20),35,4,10,1)
 sphere2 = sphere(Vec3(400,100,20),65,8,1,2)
 sphere3 = sphere(Vec3(450,450,20),45,0,1,4)
 light1 = light(Vec3(300,300,20), 8)
-light2 = light(Vec3(900,900,20), 4)
+light2 = light(Vec3(900,900,20), 20)
 
 s:add_sphere(sphere1)
 s:add_sphere(sphere2)
